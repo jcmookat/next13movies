dc21f2167991e2a9126a713bf2132fae13fbe3ca
Jose Cardenas <jscardenas717@rocketmail.com>
2023-01-31 17:56:32 +0900

    revert to my code

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,45 +1,51 @@
-import Image from "next/image"
+import Image from 'next/image'
 
-export async function generateStaticParams() {
-  const data = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
-  )
-  const res = await data.json()
-
-  return res.results.map((movie) => ({
-    movie: toString(movie.id),
-  }))
-}
+// export async function generateStaticParams() {
+// 	const data = await fetch(
+// 		`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+// 	)
+// 	const res = await data.json()
+// 	return res.results.map((movie) => ({
+// 		movie: toString(movie.id),
+// 	}))
+// }
 
 export default async function MovieDetail({ params }) {
-  const { movie } = params
-  const imagePath = "https://image.tmdb.org/t/p/original"
-  const data = await fetch(
-    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
-  )
-  const res = await data.json()
+	// console.log(params)
+	const { movie } = params //destructure params and getting 'movie'
+	const imagePath = 'https://image.tmdb.org/t/p/original'
 
-  return (
-    <div>
-      <div className="">
-        <h2 className="text-4xl">{res.title}</h2>
-        <h1 className="text-lg ">{res.release_date}</h1>
-        <h2>Runtime: {res.runtime} minutes</h2>
-        <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-lg text-sm">
-          {res.status}
-        </h2>
-        <Image
-          className="my-12 w-full"
-          src={imagePath + res.backdrop_path}
-          alt={res.title}
-          width={1000}
-          height={1000}
-          priority
-        />
-      </div>
-      <div className="my-4">
-        <p className="text-lg">{res.overview}</p>
-      </div>
-    </div>
-  )
+	// const data = await fetch(
+	// 	`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`,
+	// 	{ next: { revalidate: 60 } },
+	// )
+
+	const data = await fetch(
+		`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`,
+	)
+
+	const res = await data.json()
+	// console.log(res)
+
+	return (
+		<>
+			<div>
+				<h2 className='text-2xl'>{res.title}</h2>
+				<h2 className='text-lg'>{res.release_date}</h2>
+				<h2>Runtime: {res.runtime} minutes</h2>
+				<h2 className='bg-green-600 inline-block py-1 px-2 rounded text-sm my-2'>
+					{res.status}
+				</h2>
+				<Image
+					className='my-12 w-full'
+					src={imagePath + res.backdrop_path}
+					width={1000}
+					height={1000}
+					priority
+					alt={res.title}
+				/>
+				<p>{res.overview}</p>
+			</div>
+		</>
+	)
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,39 @@
-import "./globals.css"
-import { Montserrat } from "@next/font/google"
+import './globals.css'
+import { Montserrat, Lobster } from '@next/font/google'
 
 const montserrat = Montserrat({
-  weight: ["400"],
-  subsets: ["latin"],
-  variable: "--font-montserrat",
+	weight: ['400', '700'],
+	subsets: ['latin'],
+	variable: '--font-montserrat',
+})
+const lobster = Lobster({
+	weight: ['400'],
+	subsets: ['latin'],
+	variable: '--font-lobster',
 })
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html lang='en'>
+			{/*
         <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body className={`${montserrat.className} m-32`}>{children}</body>
-    </html>
-  )
+			<head />
+			<body className={`${montserrat.className} mx-32 my-12`}>
+				{/* <nav className={lobster.className}>
+					<h1>Logo</h1>
+					<ul>
+						<li>
+							<a href='#'>About</a>
+						</li>
+						<li>
+							<a href='#'>Sign Up</a>
+						</li>
+					</ul>
+				</nav> */}
+				{children}
+			</body>
+		</html>
+	)
 }
