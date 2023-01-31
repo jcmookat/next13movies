a607720737172441ce59f60feee11e0b90f951a3
Jose Cardenas <jscardenas717@rocketmail.com>
2023-01-31 16:36:17 +0900

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,39 @@
+import './globals.css'
+import { Montserrat, Lobster } from '@next/font/google'
+
+const montserrat = Montserrat({
+	weight: ['400', '700'],
+	subsets: ['latin'],
+	variable: '--font-montserrat',
+})
+const lobster = Lobster({
+	weight: ['400'],
+	subsets: ['latin'],
+	variable: '--font-lobster',
+})
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang='en'>
+			{/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+			<head />
+			<body className={`${montserrat.className} mx-32 my-12`}>
+				{/* <nav className={lobster.className}>
+					<h1>Logo</h1>
+					<ul>
+						<li>
+							<a href='#'>About</a>
+						</li>
+						<li>
+							<a href='#'>Sign Up</a>
+						</li>
+					</ul>
+				</nav> */}
+				{children}
+			</body>
+		</html>
+	)
+}
